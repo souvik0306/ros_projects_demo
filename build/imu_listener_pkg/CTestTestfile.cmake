199dc576e7d657ed40bd80aa341943345d1e1721
souvik0306 <souvikdatta123@gmail.com>
2025-08-30 23:45:33 +0800

    git cleanup

--- build/imu_listener_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/souvik03/Downloads/ros_projects/src/imu_listener_pkg
-# Build directory: /home/souvik03/Downloads/ros_projects/build/imu_listener_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
